d256f74af492d83eda2308e39409dab11c1be3af
TechHypeXP <104952356+TechHypeXP@users.noreply.github.com>
2026-01-13 22:16:06 +0200

    fix(config): Next.js images + logo.png

---
+++ next.config.js
@@ -0,0 +1,16 @@
+images: {
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'raw.githubusercontent.com',
+      port: '',
+      pathname: '/**',
+    },
+    {
+      protocol: 'https',
+      hostname: 'github.com',
+      port: '',
+      pathname: '/**',
+    },
+  ],
+},
